ebcaf137b404b8f0aad60bd53bcd1018bbe06ba0
박준현 (AICC코어팀) <joonhyun.park@kt.com>
2022-08-30 01:26:20 +0900

    SFP Log Check 추가

--- 검증테스트/verify.sh
+++ 검증테스트/verify.sh
@@ -56,6 +56,7 @@ ch_svc=`awk '{print $10}' $origin_path/tmp/checkverify-check_$svc`
 if [ "$svc" == "$ch_svc" ];then
 	echo "Alread In Use" > $log_TFile
 	curl $callbackurl -H "Content-Type: application/json" -d '{"resultCode":"E0090","resultMsg":"Already In Use(Verifying)","serviceCode":"'"$svc"'"}'
+	echo "{"resultCode":"E0090","resultMsg":"Already In Use(Verifying)","serviceCode":""$svc""} done" >> $log_PFile
 	exit 0
 fi
 
@@ -74,6 +75,7 @@ echo "[-------------------------------]" >> $log_PFile
 if [ ! -d $testfile_dir ] || [ ! -e $answer_dir ];then
 	echo "Check Parameter" >> $log_PFile
 	curl $callbackurl -H "Content-Type: application/json" -d '{"resultCode":"E0010","resultMsg":"Check parameter","serviceCode":"'"$svc"'"}'
+	echo "{"resultCode":"E0010","resultMsg":"Check parameter","serviceCode":""$svc""} done" >> $log_PFile
 	process_kill
 	exit 0
 fi
@@ -90,12 +92,14 @@ do
 		else
 			echo $tst1 "is not wav or pcm" >> $log_PFile
 			curl $callbackurl -H "Content-Type: application/json" -d '{"resultCode":"E0020","resultMsg":"Inputfile is not wav or pcm","serviceCode":"'"$svc"'"}'
+			echo "{"resultCode":"E0020","resultMsg":"Inputfile is not wav or pcm","serviceCode":""$svc""} done" >> $log_PFile
 			process_kill
 			exit 0
 		fi
 	else
 		echo "$tst1 is directory" >> $log_PFile
 		curl $callbackurl -H "Content-Type: application/json" -d '{"resultCode":"E0030","resultMsg":"Input is directory","serviceCode":"'"$svc"'"}'
+		echo "{"resultCode":"E0030","resultMsg":"Input is directory","serviceCode":""$svc""} done" >> $log_PFile
 		process_kill
 		exit 0
 	fi
@@ -113,19 +117,26 @@ while [ 1 ]
 do
 	nj=`tail -1 $logdir/$svc/log_verify_${svc}_${date} |head -1`
 	if [ "$nj" == "dirsim Error" ];then
-		echo "dirsim Error" >> $log_PFile
 		curl $callbackurl -H "Content-Type: application/json" -d '{"resultCode":"E0050","resultMsg":"dirsim Error","serviceCode":"'"$svc"'"}'
 		echo "{"resultCode":"E0050","resultMsg":"dirsim Error","serviceCode":""$svc""} done" >> $log_PFile
 		break
 
 	elif [ "$nj" == "Answer File Error" ];then
-		echo "Answer File Error!!" >> $log_PFile
 		curl $callbackurl -H "Content-Type: application/json" -d '{"resultCode":"E0040","resultMsg":"Answer File Error","serviceCode":"'"$svc"'"}'
 		echo "{"resultCode":"E0040","resultMsg":"Answer File Error","serviceCode":""$svc""} done" >> $log_PFile
 		break
 
 	elif [ "$nj" == "contain F sttresult error" ];then
-		echo "contain F sttresult error" >> $log_PFile
+		curl $callbackurl -H "Content-Type: application/json" -d '{"resultCode":"E0060","resultMsg":"STT Result Error","serviceCode":"'"$svc"'"}'
+		echo "{"resultCode":"E0060","resultMsg":"STT Result Error","serviceCode":""$svc""} done" >> $log_PFile
+		break
+
+	elif [ "$nj" == "filetype" ];then
+		curl -k $callbackurl -H "Content-Type: application/json" -d '{"resultCode":"E0020","resultMsg":"Check File type","serviceCode":"'"$svc"'"}'
+		echo "{"resultCode":"E0020","resultMsg":"Check File type","serviceCode":"'"$svc"'"} done" >> $log_PFile
+		break
+	
+	elif [ "$nj" == "SFP ERROR" ];then
 		curl $callbackurl -H "Content-Type: application/json" -d '{"resultCode":"E0060","resultMsg":"STT Result Error","serviceCode":"'"$svc"'"}'
 		echo "{"resultCode":"E0060","resultMsg":"STT Result Error","serviceCode":""$svc""} done" >> $log_PFile
 		break
@@ -135,15 +146,10 @@ do
 		cer1=`awk '{print $3}' $origin_path/tmp/recogRate_$svc`
 		wer1=`awk '{print $4}' $origin_path/tmp/recogRate_$svc`
 		echo "CER : $cer1 , WER : $wer1"  >> $log_PFile
+
 		curl $callbackurl -H "Content-Type: application/json" -d '{"resultCode":"C0010","resultMsg":"success","serviceCode":"'"$svc"'","cerRate":"'"$cer1"'","werRate":"'"$wer1"'"}'
 		echo "{"resultCode":"C0010","resultMsg":"success","serviceCode":""$svc"","cerRate":""$cer1"","werRate":""$wer1""} done" >> $log_PFile
-		
 		break	
-	elif [ "$nj" == "filetype" ];then
-		echo "Check File type" >> $log_PFile
-		curl -k $callbackurl -H "Content-Type: application/json" -d '{"resultCode":"E0020","resultMsg":"Check File type","serviceCode":"'"$svc"'"}'
-
-		break
 	fi
 done
 
